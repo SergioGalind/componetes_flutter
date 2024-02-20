87b86aa0af315af9e4fb82a9e3f7e92e466e2098
SergioGalindo <s.erickgalindo3@gmail.com>
2024-02-20 12:41:42 -0600

    Finishing 1st version op AppTheme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,37 +14,39 @@ class HomeScreen extends StatelessWidget {
         children:  <Widget> [
           ListTile(
             title: Text('Entradas',
-            //style: Theme.of(context).textTheme.headlineLarge,
             style: AppTheme.ligthTheme.textTheme.headlineLarge,
+            //style: Theme.of(context).textTheme.headlineLarge,
               ),
 
-            subtitle: Text('Recuperar información de TextField',
-            style: Theme.of(context).textTheme.bodySmall
-            
+            subtitle: Text(
+              'Recuperar información de TextField',
+              style: AppTheme.ligthTheme.textTheme.bodySmall,
             ),
-           
-            leading:const Icon(Icons.inbox_rounded),
-            trailing:const Icon(Icons.arrow_right_outlined),
+            leading:const Icon(Icons.inbox_rounded, color: AppTheme.primaryColor,),
+            trailing:const Icon(Icons.arrow_right_outlined, color: AppTheme.primaryColor,),
           ),
           const Divider(),
           ListTile(
             title: Text(
               'Lista infinita',
-              style: Theme.of(context).textTheme.headlineLarge,),
-            subtitle: Text('Recuperar muchos elementos',
-            style: Theme.of(context).textTheme.bodySmall),
-            leading:const Icon(Icons.list_alt_sharp),
-            trailing:const Icon(Icons.arrow_right_outlined),
+              style: AppTheme.ligthTheme.textTheme.headlineLarge,),
+            subtitle: Text(
+              'Recuperar muchos elementos',
+              style: AppTheme.ligthTheme.textTheme.bodySmall,
+              ),
+            leading:const Icon(Icons.list_alt_sharp, color: AppTheme.primaryColor,),
+            trailing:const Icon(Icons.arrow_right_outlined, color: AppTheme.primaryColor,),
           ),
           const Divider(),
           ListTile(
             title: Text(
               'Notificaciones',
-              style: Theme.of(context).textTheme.headlineLarge,),
-            subtitle: Text('Manejo de Notificaciones',
-            style: Theme.of(context).textTheme.bodySmall,),
-            leading:const Icon(Icons.notification_important),
-            trailing:const Icon(Icons.arrow_right_outlined),
+              style: AppTheme.ligthTheme.textTheme.headlineLarge,),
+            subtitle: Text(
+              'Manejo de Notificaciones',
+              style: AppTheme.ligthTheme.textTheme.bodySmall,),
+            leading:const Icon(Icons.notification_important, color: AppTheme.primaryColor,),
+            trailing:const Icon(Icons.arrow_right_outlined, color: AppTheme.primaryColor,),
           ),
         ],
       ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   //Constante de color primario
-  static const primaryColor = Color.fromARGB(255, 162, 167, 240);
+  static const primaryColor = Color.fromARGB(255, 11, 26, 245);
   //Constante de color secundario
   static const backColor = Color.fromARGB(255, 110, 142, 248);
   //Constante de color secundario
@@ -18,12 +18,18 @@ class AppTheme {
           fontSize: 26.0,
           fontWeight: FontWeight.bold,
           color: primaryColor,
+          decoration: TextDecoration.underline,
+          decorationColor: primaryColor,
+          decorationStyle: TextDecorationStyle.wavy,
+          decorationThickness: 2.0,
+          
         ),
         // fuente para textos peques 
         bodySmall: GoogleFonts.montserratAlternates(
           fontSize: 19.0,
           fontWeight: FontWeight.w500,
           color: secondaryColor,
+          fontStyle: FontStyle.italic
         ),
       ));
 }
